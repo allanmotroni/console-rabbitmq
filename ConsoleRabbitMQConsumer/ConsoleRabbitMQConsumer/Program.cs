dd9c1696aced305214d3ca1309725411772cd36b
Allan Motroni <allan.motroni@hotmail.com>
2022-06-04 08:47:29 -0300

    Primeiro commit

---
+++ ConsoleRabbitMQConsumer/ConsoleRabbitMQConsumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsoleRabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            string queueName = "fila-1";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+
+                        Console.WriteLine($" [x] Message Received: {message}");
+                    }
+                    catch (Exception ex)
+                    {                        
+                        channel.BasicNack(ea.DeliveryTag, false, false);
+                        Console.WriteLine($"Exception: {ex}");
+                    }
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: false,
+                    consumer: consumer);
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsoleRabbitMQPublisher/ConsoleRabbitMQPublisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleRabbitMQPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            string queueName = "fila-1";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                int count = 1;
+                while (true)
+                {                    
+                    string message = $"[{count++}] - Testing RabbitMQ Message";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: queueName,
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine($" [x] Message sent: {message}");
+
+                    Thread.Sleep(250);
+                }
+            }
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
